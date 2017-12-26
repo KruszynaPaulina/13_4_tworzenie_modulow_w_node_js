a9af9b55646903777b8fad9a9cb02fdfbe67dee9
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2017-12-26 22:07:18 +0100

    Add task 13.4

---
+++ modules/setTime.js
@@ -0,0 +1,18 @@
+var os = require('os');
+
+function setTime() {
+	var uptime = os.uptime();
+	var hours = Math.floor(uptime / 3600);
+	var minutes = Math.floor((uptime - (hours * 3600)) / 60);
+	var seconds = Math.floor(uptime - (hours * 3600) - (minutes * 60));
+
+	console.log('Uptime:', uptime.toFixed(), 'sec');
+	console.log('Hours, minutes and seconds: ', hours, 'h', minutes, 'min', seconds, 'sec');
+
+	var minutesx = Math.floor(uptime / 60);
+
+	console.log('Minutes and seconds: ', minutesx, 'min', seconds, 'sec');
+}
+
+exports.print = setTime;
+
